9946dcd1a9733348d6ad61b1f970d52dab2bda13
ghwzq29rnr <32108789-ghwzq29rnr@users.noreply.replit.com>
2024-02-15 07:45:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Print funksioon prindib ekraanile teksti "Tere, maailm!"
+print("Hello World")
+# \n tekitab rida alla
+print("Hello world! \nHello world")
+# + tekitab teksti vahele tühiku 
+print("Hello" + " " + "Riho")
